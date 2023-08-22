1cef927bb8d69d36022223fdd0687fdda0de7689
rushaan gandhi <rushaansps@gmail.com>
2023-08-22 18:57:26 +0530

    user entity created

---
+++ src/main/java/com/smarthome/springbootproject/models/Role.java
@@ -0,0 +1,8 @@
+package com.smarthome.springbootproject.models;
+
+public enum Role {
+	
+	ROLE_USER,	//Regular use role
+	ROLE_ADMIN	//Admin role with additional privileges
+
+}
---
+++ src/main/java/com/smarthome/springbootproject/models/User.java
@@ -0,0 +1,21 @@
+package com.smarthome.springbootproject.models;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String username;
+    private String password;
+    
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+    
+    // Getters, setters, constructors
+}
